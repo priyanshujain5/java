69c1cc613b5a25ce626db6a2d45f9ba1b3518719
Priyanshu Jain <jainpriyanshu009@gmail.com>
2025-08-02 12:44:21 +0530

    map modified

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -8,6 +8,15 @@ public class HashMapDemo {
         mp.put(3, "kc");
         System.out.println(mp);
         System.out.println(mp.get(8));
+
+        HashMap<String, Integer> fruits = new HashMap<>();
+        fruits.put("apple", 50);
+        fruits.put("banana", 10);
+        fruits.put("orange", 20);
+        fruits.put("grape", 40);
+        fruits.put("guava", 80);
+        System.out.println(fruits.size());
+
     }
 
 }
